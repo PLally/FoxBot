879855cadeae616d218c5492fad8b0fe24de7def
PLally <11771225+PLally@users.noreply.github.com>
2020-08-30 22:16:43 -0400

    fix paginator

--- commands/subscriptions/paginator.go
+++ commands/subscriptions/paginator.go
@@ -3,35 +3,38 @@ package subscriptions
 import "strings"
 
 type paginator struct {
-	builders  []strings.Builder
+	builders  []*strings.Builder
 	msgLength int
 }
 
 func (p *paginator) Line(s string) {
-	if len(p.builders) == 0 {
-		var newBuilder strings.Builder
-		newBuilder.WriteString("```")
-		p.builders = append(p.builders, newBuilder)
-	}
+	var builder *strings.Builder
 
 	p.msgLength = p.msgLength + len(s) + 1
-	builder := p.builders[len(p.builders)-1]
-	if p.msgLength > 1990 {
+	if len(p.builders) == 0 {
+		builder = p.newBuilder()
+	} else if p.msgLength > 1985 {
 		builder.WriteString("```")
 		p.msgLength = 0
-		var newBuilder strings.Builder
-		newBuilder.WriteString("```")
-		p.builders = append(p.builders, newBuilder)
+		builder = p.newBuilder()
+	} else {
+		builder =  p.builders[len(p.builders)-1]
 	}
 
-	builder = p.builders[len(p.builders)-1]
 	builder.WriteString(s)
 	builder.WriteByte('\n')
 
+}
 
+func (p *paginator) newBuilder() *strings.Builder {
+	var newBuilder strings.Builder
+	newBuilder.WriteString("```\n")
+	p.builders = append(p.builders, &newBuilder)
+	return &newBuilder
 }
 
 func (p *paginator) GetMessages() []string {
+	p.builders[len(p.builders)-1].WriteString("```")
 	var messages []string
 	for _, builder := range p.builders {
 		messages = append(messages, builder.String())
